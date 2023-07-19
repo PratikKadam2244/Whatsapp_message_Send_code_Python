b68bbc9abce59708c2e927736ccc64fcfbb22771
Pratik Kadam <123864464+PratikKadam2244@users.noreply.github.com>
2023-07-19 11:52:33 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,16 @@
+#----------1st Method ---------#
+import pyautogui
+import time
+
+time.sleep(4)
+count=0
+while count<=2:
+    pyautogui.typewrite("Happy Birthday Darling🍦🥧😆")
+    pyautogui.press("enter")
+    count=count+1
+
+#-------2nd Method----------#
+
+import pywhatkit
+pywhatkit.sendwhatmsg('+917887981348','Bete kidar hai',22,1)
+
